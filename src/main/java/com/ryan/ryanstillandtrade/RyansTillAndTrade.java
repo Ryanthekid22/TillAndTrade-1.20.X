5064ab375a89b769bc50afb3b0598f474414974d
Ryanthekid22 <Formiatti@gmail.com>
2023-12-09 16:52:43 +1000

    base template

--- src/main/java/com/ryan/ryanstillandtrade/RyansTillAndTrade.java
+++ src/main/java/com/ryan/ryanstillandtrade/RyansTillAndTrade.java
@@ -1,5 +1,7 @@
 package com.ryan.ryanstillandtrade;
 
+import com.ryan.ryanstillandtrade.block.ModBlocks;
+import com.ryan.ryanstillandtrade.item.ModItemGroups;
 import com.ryan.ryanstillandtrade.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -7,14 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RyansTillAndTrade implements ModInitializer {
-
 	public static final String MOD_ID = "ryanstillandtrade";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 	}
 }
--- src/main/java/com/ryan/ryanstillandtrade/item/ModItems.java
+++ src/main/java/com/ryan/ryanstillandtrade/item/ModItems.java
@@ -2,10 +2,7 @@ package com.ryan.ryanstillandtrade.item;
 
 import com.ryan.ryanstillandtrade.RyansTillAndTrade;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,17 +10,12 @@ import net.minecraft.util.Identifier;
 public class ModItems {
 
     public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings()));
-
-    private static void addItemsToIngredientGroup(FabricItemGroupEntries entries){
-        entries.add(TOMATO);
-    }
+    public static final Item MANGO = registerItem("mango", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(RyansTillAndTrade.MOD_ID, name), item);
     }
     public static void registerModItems(){
         RyansTillAndTrade.LOGGER.info("Registering Mod Items for " + RyansTillAndTrade.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientGroup);
     }
 }
