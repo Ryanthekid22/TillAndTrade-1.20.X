7c97a00e5d3b907f49e3d8eb67719132d3e05d07
Ryanthekid22 <Formiatti@gmail.com>
2023-12-14 12:19:05 +1000

    FARMING

--- src/main/java/com/ryan/ryanstillandtrade/datagen/ModRecipeProvider.java
+++ src/main/java/com/ryan/ryanstillandtrade/datagen/ModRecipeProvider.java
@@ -5,7 +5,10 @@ import com.ryan.ryanstillandtrade.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
     public ModRecipeProvider(FabricDataOutput output) {
@@ -15,5 +18,32 @@ public class ModRecipeProvider extends FabricRecipeProvider {
     @Override
     public void generate(RecipeExporter exporter) {
         offerShapelessRecipe(exporter, ModBlocks.MANGO_PLANKS, ModBlocks.MANGO_LOG, "RYANSTILLANDTRADE_GROUP", 4);
+
+        //ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.DIAMOND_SCYTHE, 1)
+        //        .pattern("b")
+        //        .pattern("s")
+        //        .pattern("s")
+        //        .input('s', Items.STICK)
+        //        .input('b', ModItems.DIAMOND_SCYTHE_BLADE)
+        //        .criterion(hasItem(ModItems.DIAMOND_SCYTHE_BLADE), conditionsFromItem(ModItems.DIAMOND_SCYTHE_BLADE))
+        //        .offerTo(exporter, new Identifier(getRecipeName(ModItems.DIAMOND_SCYTHE)));
+
+        //ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.GOLD_SCYTHE, 1)
+        //        .pattern("b")
+        //        .pattern("s")
+        //        .pattern("s")
+        //        .input('s', Items.STICK)
+        //        .input('b', ModItems.GOLD_SCYTHE_BLADE)
+        //        .criterion(hasItem(ModItems.GOLD_SCYTHE_BLADE), conditionsFromItem(ModItems.GOLD_SCYTHE_BLADE))
+        //        .offerTo(exporter, new Identifier(getRecipeName(ModItems.GOLD_SCYTHE)));
+
+        //ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.IRON_SCYTHE, 1)
+        //        .pattern("b")
+        //        .pattern("s")
+        //        .pattern("s")
+        //        .input('s', Items.STICK)
+        //        .input('b', ModItems.IRON_SCYTHE_BLADE)
+        //        .criterion(hasItem(ModItems.IRON_SCYTHE_BLADE), conditionsFromItem(ModItems.IRON_SCYTHE_BLADE))
+        //        .offerTo(exporter, new Identifier(getRecipeName(ModItems.IRON_SCYTHE)));
     }
 }
