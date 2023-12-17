f8d0a15dc6c5eb71952f94165df538cc22086cd3
Ryanthekid22 <Formiatti@gmail.com>
2023-12-17 15:48:04 +1000

    WORKING

--- src/main/java/com/ryan/ryanstillandtrade/datagen/ModRecipeProvider.java
+++ src/main/java/com/ryan/ryanstillandtrade/datagen/ModRecipeProvider.java
@@ -6,9 +6,11 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.AbstractCookingRecipe;
 import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.ShapedRecipe;
 import net.minecraft.recipe.SmeltingRecipe;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
@@ -50,6 +52,32 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .offerTo(exporter, new Identifier(getRecipeName(ModItems.IRON_SCYTHE)));
 
         offerNetheriteUpgradeRecipe(exporter, ModItems.DIAMOND_SCYTHE,RecipeCategory.TOOLS,ModItems.NETHERITE_SCYTHE);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.IRON_KNIFE, 1)
+                .pattern("b")
+                .pattern("s")
+                .input('s', Items.STICK)
+                .input('b', Items.IRON_INGOT)
+                .criterion(hasItem(Items.IRON_INGOT), conditionsFromItem(Items.IRON_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.IRON_KNIFE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.GOLD_KNIFE, 1)
+                .pattern("b")
+                .pattern("s")
+                .input('s', Items.STICK)
+                .input('b', Items.GOLD_INGOT)
+                .criterion(hasItem(Items.GOLD_INGOT), conditionsFromItem(Items.GOLD_INGOT))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.GOLD_KNIFE)));
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, ModItems.DIAMOND_KNIFE, 1)
+                .pattern("b")
+                .pattern("s")
+                .input('s', Items.STICK)
+                .input('b', Items.DIAMOND)
+                .criterion(hasItem(Items.DIAMOND), conditionsFromItem(Items.DIAMOND))
+                .offerTo(exporter, new Identifier(getRecipeName(ModItems.DIAMOND_KNIFE)));
+
+        offerNetheriteUpgradeRecipe(exporter, ModItems.DIAMOND_KNIFE,RecipeCategory.TOOLS,ModItems.NETHERITE_KNIFE);
 
     }
 }
